a5186de8867f6b197f556e35ad88d29dbc1cc244
bashi8128 <itabasi.lm@gmail.com>
2022-04-03 14:06:46 +0900

    Change the types of widget for addresses in fyne window

--- subnet_calculator.go
+++ subnet_calculator.go
@@ -7,12 +7,15 @@ import (
   "fyne.io/fyne/v2"
   "fyne.io/fyne/v2/app"
   "fyne.io/fyne/v2/container"
+  "fyne.io/fyne/v2/data/binding"
   "fyne.io/fyne/v2/widget"
   "github.com/atotto/clipboard"
 
 )
 
 func main() {
+  var IPAddrEntry, NWAddrEntry, BCAddrEntry *widget.Entry
+
   readText, err := clipboard.ReadAll()
 
   if err != nil {
@@ -24,10 +27,46 @@ func main() {
   myApp := app.New()
   myWindow := myApp.NewWindow("Subnet Calculator")
 
+  IPAddrBound := binding.NewString()
+  if IPAddr != nil {
+    IPAddrBound.Set(IPAddr.String())
+    IPAddrEntry = widget.NewEntryWithData(IPAddrBound)
+  } else {
+    IPAddrEntry = widget.NewEntry()
+  }
+  IPAddrContainer := container.NewHBox(
+    widget.NewLabel("IP Address"),
+    IPAddrEntry,
+  )
+
+  NWAddrBound := binding.NewString()
+  if NWAddr != nil {
+    NWAddrBound.Set(NWAddr.String())
+    NWAddrEntry = widget.NewEntryWithData(NWAddrBound)
+  } else {
+    NWAddrEntry = widget.NewEntry()
+  }
+  NWAddrContainer := container.NewHBox(
+    widget.NewLabel("Network Address"),
+    NWAddrEntry,
+  )
+
+  BCAddrBound := binding.NewString()
+  if BCAddr != nil {
+    BCAddrBound.Set(BCAddr.String())
+    BCAddrEntry = widget.NewEntryWithData(BCAddrBound)
+  } else {
+    BCAddrEntry = widget.NewEntry()
+  }
+  BCAddrContainer := container.NewHBox(
+    widget.NewLabel("Network Address"),
+    BCAddrEntry,
+  )
+
   myWindow.SetContent(container.NewVBox(
-    widget.NewLabel("IP Address: " + IPAddr.String()),
-    widget.NewLabel("Network Address: " + NWAddr.String()),
-    widget.NewLabel("Broadcast Address: " + BCAddr.String()),
+    IPAddrContainer,
+    NWAddrContainer,
+    BCAddrContainer,
   ))
 
   myWindow.Resize(fyne.NewSize(500, 100))
